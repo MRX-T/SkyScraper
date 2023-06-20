8f19d84e9f5c5088ed03c68d025566e8939d7f12
Twix <40114067+MRX-T@users.noreply.github.com>
2023-06-20 17:58:28 +0200

    update file

---
+++ src/SkyScraper.Tests/ImageScraperObserverFixtures/When_html_contains_two_identical_images.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace SkyScraper.Tests.ImageScraperObserverFixtures
+{
+    [TestFixture]
+    class When_html_contains_two_identical_images : ConcernForImageScraperObserverOnNext
+    {
+        protected override void Context()
+        {
+            base.Context();
+            HtmlDoc.Html = @"<html>
+                         <img src=""image.png"" />
+                         <img src=""image.png"" />
+                         </html>";
+            HtmlDoc.Uri = new Uri("http://test/");
+            HttpClient.GetByteArray(Arg.Any<Uri>()).Returns(new Task<byte[]>(() => new byte[0]));
+        }
+
+        [Test]
+        public void Then_http_client_should_download_image_once()
+        {
+            HttpClient.Received(1).GetByteArray(Arg.Is<Uri>(x => x.ToString() == "http://test/image.png"));
+        }
+    }
+}
