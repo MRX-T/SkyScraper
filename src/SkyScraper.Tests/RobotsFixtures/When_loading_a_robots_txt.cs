0c43e8e73880b65c82b7bcddd02e9ae10ff4c306
Twix <40114067+MRX-T@users.noreply.github.com>
2023-06-20 17:58:32 +0200

    update file

---
+++ src/SkyScraper.Tests/RobotsFixtures/When_loading_a_robots_txt.cs
@@ -0,0 +1,67 @@
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.IO;
+
+namespace SkyScraper.Tests.RobotsFixtures
+{
+    [TestFixture]
+    class When_loading_a_robots_txt
+    {
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            var path = Path.Combine(Environment.CurrentDirectory, "RobotsFixtures\\robots.txt");
+            var robots = File.OpenText(path).ReadToEnd();
+            Robots.Load(robots);
+        }
+
+        [Test]
+        public void Then_path1_should_not_be_allowed()
+        {
+            Robots.PathIsAllowed("/path1").Should().BeFalse();
+        }
+
+        [Test]
+        public void Then_path1_foo_should_not_be_allowed()
+        {
+            Robots.PathIsAllowed("/path1/foo").Should().BeFalse();
+        }
+
+        [Test]
+        public void Then_path2_should_not_be_allowed()
+        {
+            Robots.PathIsAllowed("/path2").Should().BeFalse();
+        }
+
+        [Test]
+        public void Then_botpath1_should_be_allowed()
+        {
+            Robots.PathIsAllowed("/botpath1").Should().BeTrue();
+        }
+
+        [Test]
+        public void Then_foo_path3_should_not_be_allowed()
+        {
+            Robots.PathIsAllowed("/foo/path3").Should().BeFalse();
+        }
+
+        [Test]
+        public void Then_path1_filetxt_should_be_allowed()
+        {
+            Robots.PathIsAllowed("/path1/file.txt").Should().BeTrue();
+        }
+
+        [Test]
+        public void Then_login_should_not_be_allowed()
+        {
+            Robots.PathIsAllowed("/login.html?ok=1").Should().BeFalse();
+        }
+
+        [Test]
+        public void Then_sitemap_should_be_set()
+        {
+            Robots.SiteMap.Should().Be("http://foo/sitemap.xml");
+        }
+    }
+}
