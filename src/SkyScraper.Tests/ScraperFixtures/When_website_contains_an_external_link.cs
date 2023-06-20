a525c11cf4f58f3332a393aee47ce916ca7c1df5
Twix <40114067+MRX-T@users.noreply.github.com>
2023-06-20 17:58:46 +0200

    update file

---
+++ src/SkyScraper.Tests/ScraperFixtures/When_website_contains_an_external_link.cs
@@ -0,0 +1,45 @@
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SkyScraper.Tests.ScraperFixtures
+{
+    [TestFixture]
+    class When_website_contains_an_external_link : ConcernForScraper
+    {
+        readonly List<HtmlDoc> htmlDocs = new List<HtmlDoc>();
+        string page;
+
+        protected override void Context()
+        {
+            base.Context();
+            Uri = new Uri("http://test");
+            page = @"<html>
+                         <a href=""http://foo"">link1</a>
+                         </html>";
+            HttpClient.GetString(Uri).Returns(Task.Factory.StartNew(() => page));
+            OnNext = x => htmlDocs.Add(x);
+        }
+
+        [Test]
+        public void Then_htmldocs_should_contain_home_page()
+        {
+            htmlDocs.Should().Contain(x => x.Uri.ToString() == "http://test/" && x.Html == page);
+        }
+
+        [Test]
+        public void Then_link_should_not_be_scraped()
+        {
+            HttpClient.DidNotReceive().GetString(Arg.Is<Uri>(x => x.ToString() == "http://foo"));
+        }
+
+        [Test]
+        public void Then_one_htmldoc_should_be_returned()
+        {
+            htmlDocs.Count.Should().Be(1);
+        }
+    }
+}
